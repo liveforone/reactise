d8e0ec17bae53eaf3c9744b59225d9c60db7eee2
liveforone <yc4852@gmail.com>
2025-11-04 11:11:05 +0900

    Refactor : route uses prefix func for simple.

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,9 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 import { UsersClientApi } from "./routes/api/UsersClientApi";
 import { PostClientApi } from "./routes/api/PostClientApi";
 
@@ -12,17 +17,19 @@ import { PostClientApi } from "./routes/api/PostClientApi";
 export default [
   //home
   index("routes/home.tsx"),
-  //users
-  route(UsersClientApi.LOGIN, "routes/account/Login.tsx"),
-  route(UsersClientApi.LOGOUT, "routes/account/Logout.tsx"),
-  route(UsersClientApi.WITHDRAW, "routes/account/Withdraw.tsx"),
-  route(UsersClientApi.SIGNUP, "routes/users/Signup.tsx"),
-  route(UsersClientApi.PROFILE, "routes/users/Profile.tsx"),
-  route(UsersClientApi.UPDAET_PASSWORD, "routes/users/UpdatePassword.tsx"),
-  //post
-  route(PostClientApi.HOME, "routes/post/PostHome.tsx"),
-  route(PostClientApi.CREATE, "routes/post/CreatePost.tsx"),
-  route("/posts/:id", "routes/post/PostDetail.tsx"),
-  route("/posts/update/:id", "routes/post/UpdatePost.tsx"),
-  route("/posts/belong-writer/:writerId", "routes/post/PostBelongWriter.tsx"),
+  ...prefix("users", [
+    route("login", "routes/account/Login.tsx"),
+    route("logout", "routes/account/Logout.tsx"),
+    route("withdraw", "routes/account/Withdraw.tsx"),
+    route("signup", "routes/users/Signup.tsx"),
+    route("profile", "routes/users/Profile.tsx"),
+    route("update/password", "routes/users/UpdatePassword.tsx"),
+  ]),
+  ...prefix("posts", [
+    index("routes/post/PostHome.tsx"),
+    route(":id", "routes/post/PostDetail.tsx"),
+    route("create", "routes/post/CreatePost.tsx"),
+    route("update/:id", "routes/post/UpdatePost.tsx"),
+    route("belong-writer/:writerId", "routes/post/PostBelongWriter.tsx"),
+  ]),
 ] satisfies RouteConfig;
